c6d1aa023bfad626ecc8ea7100dac5f149416025
88plug <19512127+88plug@users.noreply.github.com>
2020-11-10 18:45:30 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone https://github.com/ToddAtkins/swatchdog.git
+cd swatchdog/
+sudo perl -MCPAN -e 'install Date::Calc'
+sudo perl -MCPAN -e 'install Date::Manip'
+sudo perl -MCPAN -e 'install File::Tail'
+perl Makefile.PL
+make
+sudo make install
+sudo make realclean
