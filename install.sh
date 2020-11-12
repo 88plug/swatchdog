7324def905d6eccfdc1bbf6f229ed684ea3211a0
88plug <19512127+88plug@users.noreply.github.com>
2020-11-12 01:44:09 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/ToddAtkins/swatchdog.git
 cd swatchdog/
 sudo perl -MCPAN -e 'install Date::Calc'
+sudo perl -MCPAN -e 'install Date::Parse'
 sudo perl -MCPAN -e 'install Date::Manip'
 sudo perl -MCPAN -e 'install File::Tail'
 perl Makefile.PL
